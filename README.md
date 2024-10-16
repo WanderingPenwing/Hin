8ba4e6219699aa1d5ce621d5b7c97d214e805753
WanderingPenwing <nicolas.pinson31@gmail.com>
2024-10-16 16:23:29 +0200

    multi subject

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Hin
 
-A searxng/BERT mix to find science papers more efficiently, given a subject
+A searxng/BERT mix to find science papers more efficiently, given a subject. It mixes AI and search engines, taking inspiration from perplexity.ai
+
+## Work in progress
+
+You can sniff around in the code, but it is nowhere near where I want it to be.
--- main.py
+++ main.py
@@ -6,35 +6,56 @@ from src.scrub import scrub_web
 from src.key3 import create_queries
 from src.evaluate import sort_results, CLS_POOLING, MEAN_POOLING, MAX_POOLING
 
-def hin_fetch(subject, weights, pooling):
+def hin_fetch(subjects, weights, pooling):
     current_time = datetime.now().strftime("%m-%d_%H-%M")
-    data_path = f"web_data/{hash(subject)}.json"
     file_path = f"logs/run_{current_time}_{weights}{pooling}.md"
-    log_content = f"# Hin run, {current_time}\n\nSubject : {subject}\n\n"
 
     results = []
+
+    for subject in subjects :
+        log_content = f"# Hin run, {current_time}\n\nSubject : {subject}\n\n"
+        
+        data_path = f"web_data/{hash(subject)}.json"
     
-    if os.path.exists(data_path) :
-        log_content += f"## Query results from {data_path}*\n\n"
-        print(f"* Subject known from {data_path}")
+        if os.path.exists(data_path) :
+            log_content += f"## Query results from {data_path}*\n\n"
+            print(f"* Subject known from {data_path}")
+            
+            with open(data_path, 'r', encoding='utf-8') as f:
+                subject_results = json.load(f)
+        else :
+            queries, keyword_log = create_queries(subject)
+            log_content += keyword_log
+
+            subject_results, scrub_log = scrub_web(queries)
+            log_content += scrub_log
+
+            with open(data_path, 'w', encoding='utf-8') as f:
+                json.dump(subject_results, f, ensure_ascii=False, indent=4)
+
+            log_content += f"*Stored results in {data_path}*\n\n"
+            print(f"\n* Stored results in {data_path}")
+
+        results += subject_results
+
+    print("N results :", len(results))
+
+    full_scored_results = []
         
-        with open(data_path, 'r', encoding='utf-8') as f:
-            results = json.load(f)
-    else :
-        queries, keyword_log = create_queries(subject)
-        log_content += keyword_log
-
-        results, scrub_log = scrub_web(queries)
-        log_content += scrub_log
-
-        with open(data_path, 'w', encoding='utf-8') as f:
-            json.dump(results, f, ensure_ascii=False, indent=4)
-
-        log_content += f"*Stored results in {data_path}*\n\n"
-        print(f"\n* Stored results in {data_path}")
+    for subject in subjects :
+        print("- Subject", subject)
+        scored_results, results_log = sort_results(subject, results, weights, pooling)
+        log_content += results_log
         
-    sorted_results, results_log = sort_results(subject, results, weights, pooling)
-    log_content += results_log
+        for result in scored_results :
+            for full_result in full_scored_results:
+                if full_result['title'] == result['title']:
+                    full_result['score'] += result['score']
+                    break
+            else :
+                full_scored_results.append(result)
+
+    sorted_results = sorted(full_scored_results, key=lambda x: x['score'], reverse=True)
 
     print("### Done ###\n")
 
@@ -53,13 +74,17 @@ def hin_fetch(subject, weights, pooling):
 #subject = "State of the art on the identification of wood structure natural frequencies. Influence of the mechanical properties and interest in sensitivity analysis as prospects for reverse identification method of wood elastic properties."
 #subject = "Experiments, numerical models and optimization of carbon-epoxy plates damped by a frequency-dependent interleaved viscoelastic layer"
 #subject = "Dynamic response of carbon-epoxy laminates including a perforated viscoelastic film"
-subject = "tig welding of inconel 625 and influences on micro structures"
+#subject = "tig welding of inconel 625 and influences on micro structures"
+#subject = "Modelisation of thermo-mechanical impact of temperature oscillations on bonding and metallization for SiC MOSFETs soldered on ceramic substrate using ANSYS"
 #subject = "Thermo-Mechanical Impact of temperature oscillations on bonding and metallization for SiC MOSFETs soldered on ceramic substrate"
+subjects = [
+    "Impact response of carbon-epoxy plates damped by a interleaved perforated viscoelastic layer",
+    "Transient response of carbon-epoxy plates damped by a interleaved perforated viscoelastic layer",
+    "Shock response of carbon-epoxy plates damped by a interleaved perforated viscoelastic layer",
+    "Fracture of composite laminate with an inserted perforated viscoelastic layer",
+    "Impact response of composite laminate damped by a interleaved perforated viscoelastic layer",
+]
+
 
 # hin_fetch(subject, [title_weight, snippet_weight], [title_pooling, snippet_pooling])
-hin_fetch(subject, [2,1], [CLS_POOLING, MAX_POOLING])
-#hin_fetch(subject, [1,0], [MEAN_POOLING,MAX_POOLING])
-#hin_fetch(subject, [1,0], [MAX_POOLING, MAX_POOLING])
-#hin_fetch(subject, [0,1], [CLS_POOLING, CLS_POOLING])
-#hin_fetch(subject, [0,1], [CLS_POOLING, MEAN_POOLING])
-#hin_fetch(subject, [0,1], [CLS_POOLING, MAX_POOLING])
+hin_fetch(subjects, [2,1], [CLS_POOLING, MAX_POOLING])
